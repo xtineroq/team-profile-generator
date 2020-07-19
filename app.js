4af5929014dfe8693434a0a7a11965f62c658188
xtineroq <mcroque89@gmail.com>
2020-07-19 17:05:02 +1000

    fixed render issue

--- app.js
+++ app.js
@@ -4,22 +4,18 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-const util = require("util");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
-const writeFileAsync = util.promisify(fs.writeFile);
-
 const employeeInfo = [];
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
 
-generateHTML();
+addTeamMember();
 
 async function addTeamMember() {
+    // prompt common info to all employees
     await inquirer.prompt([
         {
             type: "input",
@@ -48,6 +44,7 @@ async function addTeamMember() {
         }
     ])
     .then(async function({name, role, id, email}) {
+        // prompt info specific to each employee type
         if (role === "Engineer") {
             await inquirer.prompt([
                 {
@@ -57,7 +54,9 @@ async function addTeamMember() {
                 }
             ])
             .then(function({github}) {
+                // create a new array of engineer
                 const newEngineer = new Engineer(name, id, email, github);
+                // push object to array holder
                 employeeInfo.push(newEngineer);
             });
         } else if (role === "Intern") {
@@ -69,7 +68,9 @@ async function addTeamMember() {
                 }
             ])
             .then(function({school}) {
+                // create a new array of intern
                 const newIntern = new Intern(name, id, email, school);
+                // push object to array holder
                 employeeInfo.push(newIntern);
             });
         } else {
@@ -81,12 +82,15 @@ async function addTeamMember() {
                 }
             ])
             .then(function({officeNumber}) {
+                // create a new array of manager
                 const newManager = new Manager(name, id, email, officeNumber);
+                // push object to array holder
                 employeeInfo.push(newManager);
             });
         }
     })
     .then(async function() {
+        // prompt if user would like to add another employee
         await inquirer.prompt([
             {
                 type: "list",
@@ -100,48 +104,19 @@ async function addTeamMember() {
         ])
         .then(function({anotherTM}) {
             if (anotherTM === "yes") {
+                // start over
                 addTeamMember();
             } else {
+                // generate html
+                fs.writeFile(outputPath, render(employeeInfo), function(err){
+                    if(err) {
+                        return console.log(err);
+                    } else {
+                        console.log("You've successfully generated a Team Profile!");
+                    }
+                });
                 return;
             }
         });
     });
 }
-
-
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-async function generateHTML() {
-    try {
-        await addTeamMember();
-        await writeFileAsync(outputPath, render(employeeInfo));
-        console.log("You've successfully generated a Team Profile!");
-
-    //catch the error
-    } catch (err) {
-        console.log(err);
-    }
-}
-
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
-
-
